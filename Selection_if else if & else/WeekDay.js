c62b8ad190e41604081a2ee590121560b06d5b24
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-20 18:41:05 +0530

    [Roshni]-Selection problem with If else if and else

---
+++ Selection_if else if & else/WeekDay.js
@@ -0,0 +1,35 @@
+// Read a Number and Display the week day 
+let number = Math.floor(Math.random()*7) + 1;
+console.log("Number : "+number);
+if (number == 1) 
+{
+    console.log("Sunday");
+} 
+else if (number == 2) 
+{
+    console.log("Monday");
+} 
+else if (number == 3) 
+{
+    console.log("Tuesday");
+} 
+else if (number == 4) 
+{
+    console.log("Wednesday");
+} 
+else if (number == 5) 
+{
+    console.log("Thursday");
+} 
+else if (number == 6) 
+{
+    console.log("Friday");
+} 
+else if (number == 7) 
+{
+    console.log("Saturday");
+} 
+else 
+{
+    console.log("Invalid Input");
+}
