bcd794d0a4bc99de094bc9444510588c59dc84c3
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-20 17:53:43 +0530

    [Roshni]-Selection problem with If and Else

---
+++ Selection_if & else/LeapYear.js
@@ -0,0 +1,11 @@
+// Check input Year is a Leap Year or not.
+let Year = (Math.floor(Math.random() *9000)+1000); 
+console.log("Year : "+Year);
+CheckLeap1=Year % 400;
+CheckLeap2=Year % 100;
+CheckLeap3=Year % 4;
+if (CheckLeap1 == 0 || CheckLeap2 !=0 && CheckLeap3 == 0)
+{
+    console.log("Given year is a Leap year.");
+}
+else console.log("Given year is not a Leap year.");
